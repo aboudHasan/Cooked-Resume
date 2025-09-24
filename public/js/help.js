7f40c9eaeadf36264f72fc0223a66cad8969ec7c
aboudHasan <aboudhasan06@gmail.com>
2025-09-24 01:56:26 -0400

    minor fixes

--- public/js/help.js
+++ public/js/help.js
@@ -59,7 +59,7 @@ contactForm.addEventListener("submit", async (e) => {
 
   const formData = new FormData(contactForm);
 
-  const res = await fetch("http://localhost:8080/api/contact", {
+  const res = await fetch("http://cooked-vercel.app/api/contact", {
     method: "POST",
     body: formData,
   });
